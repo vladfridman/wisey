a8aef5786e57ecfda6c6c525fec1876346554ae6
Vlad Fridman <vlad.fridman@gmail.com>
2016-12-07 01:48:37 +0300

    Sort includes

--- codegen.hpp
+++ codegen.hpp
@@ -1,23 +1,22 @@
 #include <stack>
-#include <llvm/Bitcode/BitstreamWriter.h>
-#include <llvm/IR/Module.h>
-#include <llvm/IR/Function.h>
-#include <llvm/IR/Type.h>
-#include <llvm/IR/DerivedTypes.h>
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/LegacyPassManager.h>
-#include <llvm/IR/Instructions.h>
-#include <llvm/IR/CallingConv.h>
-#include <llvm/IR/Verifier.h>
-#include <llvm/IR/IRPrintingPasses.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Module.h>
-#include "llvm/IR/TypeBuilder.h"
 #include <llvm/ADT/Twine.h>
-#include <llvm/Support/TargetSelect.h>
+#include <llvm/Bitcode/BitstreamWriter.h>
 #include <llvm/ExecutionEngine/GenericValue.h>
 #include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/IR/CallingConv.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/IRPrintingPasses.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Type.h>
+#include "llvm/IR/TypeBuilder.h"
+#include <llvm/IR/Verifier.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/TargetSelect.h>
 #include "llvm/Transforms/Scalar.h"
 
 using namespace llvm;
