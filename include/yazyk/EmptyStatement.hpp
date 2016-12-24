0a4b7b1f8d8183272e10a77d9ff62bdd7c1dedb3
Vladimir Fridman <vlad.fridman@gmail.com>
2016-12-24 15:47:56 -0800

    Rename EmptyExpressionStatement into EmptyStatement

--- include/yazyk/EmptyStatement.hpp
+++ include/yazyk/EmptyStatement.hpp
@@ -1,13 +1,13 @@
 //
-//  EmptyExpressionStatement.hpp
+//  EmptyStatement.hpp
 //  Yazyk
 //
 //  Created by Vladimir Fridman on 12/24/16.
 //  Copyright © 2016 Vladimir Fridman. All rights reserved.
 //
 
-#ifndef EmptyExpressionStatement_h
-#define EmptyExpressionStatement_h
+#ifndef EmptyStatement_h
+#define EmptyStatement_h
 
 #include "yazyk/IExpression.hpp"
 #include "yazyk/IStatement.hpp"
@@ -17,16 +17,16 @@ namespace yazyk {
 /**
  * Represents an empty statement that does not do anything
  */
-class EmptyExpressionStatement : public IStatement {
+class EmptyStatement : public IStatement {
   
 public:
-  EmptyExpressionStatement() { }
+  EmptyStatement() { }
   
-  ~EmptyExpressionStatement() { }
+  ~EmptyStatement() { }
   
   llvm::Value* generateIR(IRGenerationContext& context) override { return NULL; }
 };
   
 } /* namespace yazyk */
 
-#endif /* EmptyExpressionStatement_h */
+#endif /* EmptyStatement_h */
---
+++ tests/TestEmptyStatement.cpp
@@ -0,0 +1,28 @@
+//
+//  TestEmptyStatement.cpp
+//  Yazyk
+//
+//  Created by Vladimir Fridman on 12/24/16.
+//  Copyright © 2016 Vladimir Fridman. All rights reserved.
+//
+//  Tests {@link EmptyStatement}
+//
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "TestFileSampleRunner.hpp"
+#include "yazyk/EmptyStatement.hpp"
+
+using namespace yazyk;
+
+TEST(TestEmptyStatement, SimpleEmptyStatementTest) {
+  IRGenerationContext context;
+  EmptyStatement statement;
+  
+  EXPECT_EQ(statement.generateIR(context) == NULL, true);
+}
+
+TEST_F(TestFileSampleRunner, EmptyStatementRunTest) {
+  runFile("tests/samples/test_empty_statement.yz", "5");
+}
