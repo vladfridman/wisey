caf073d6293080abe9cba272abad1f5bf6e729dd
Vladimir Fridman <vlad.fridman@gmail.com>
2017-03-07 17:10:15 +0300

    Add InterfaceTypeSpecifier class

---
+++ include/yazyk/InterfaceTypeSpecifier.hpp
@@ -0,0 +1,13 @@
+//
+//  InterfaceTypeSpecifier.hpp
+//  Yazyk
+//
+//  Created by Vladimir Fridman on 3/7/17.
+//  Copyright © 2017 Vladimir Fridman. All rights reserved.
+//
+
+#ifndef InterfaceTypeSpecifier_h
+#define InterfaceTypeSpecifier_h
+
+
+#endif /* InterfaceTypeSpecifier_h */
---
+++ tests/TestInterfaceTypeSpecifier.cpp
@@ -0,0 +1,9 @@
+//
+//  TestInterfaceTypeSpecifier.cpp
+//  Yazyk
+//
+//  Created by Vladimir Fridman on 3/7/17.
+//  Copyright © 2017 Vladimir Fridman. All rights reserved.
+//
+
+#include <stdio.h>
