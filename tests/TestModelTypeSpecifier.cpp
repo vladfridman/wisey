93f5be30de0a56728fdc65396f274384afbffdbc
Vladimir Fridman <vlad.fridman@gmail.com>
2017-03-07 17:24:58 +0300

    Improve ModelTypeSpecifierTest

--- tests/TestModelTypeSpecifier.cpp
+++ tests/TestModelTypeSpecifier.cpp
@@ -12,12 +12,36 @@
 #include <gmock/gmock.h>
 
 #include "yazyk/ModelTypeSpecifier.hpp"
+#include "yazyk/PrimitiveTypes.hpp"
 
 using namespace llvm;
+using namespace std;
 using namespace yazyk;
 
 TEST(ModelTypeSpecifierTest, CreationTest) {
-  ModelTypeSpecifier modelTypeSpecifier("a");
+  IRGenerationContext context;
+  LLVMContext& llvmContext = context.getLLVMContext();
   
-  EXPECT_EQ(modelTypeSpecifier.getName(), "a");
+  vector<Type*> types;
+  types.push_back(Type::getInt32Ty(llvmContext));
+  types.push_back(Type::getInt32Ty(llvmContext));
+  StructType* structType = StructType::create(llvmContext, "MSquare");
+  structType->setBody(types);
+  map<string, ModelField*>* fields = new map<string, ModelField*>();
+  ModelField* widthField = new ModelField(PrimitiveTypes::INT_TYPE, 0);
+  ModelField* heightField = new ModelField(PrimitiveTypes::INT_TYPE, 1);
+  (*fields)["width"] = widthField;
+  (*fields)["height"] = heightField;
+  vector<MethodArgument*> methodArguments;
+  Method* method = new Method("foo", PrimitiveTypes::INT_TYPE, methodArguments);
+  map<string, Method*>* methods = new map<string, Method*>();
+  (*methods)["foo"] = method;
+  vector<Interface*> interfaces;
+  Model* model = new Model("MSquare", structType, fields, methods, interfaces);
+  context.addModel(model);
+
+  ModelTypeSpecifier modelTypeSpecifier("MSquare");
+  
+  EXPECT_EQ(modelTypeSpecifier.getName(), "MSquare");
+  EXPECT_EQ(modelTypeSpecifier.getType(context), model);
 }
