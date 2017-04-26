dcc989747d7dd6576b9acaa70e234396faffb96d
Vladimir Fridman <vlad.fridman@gmail.com>
2017-04-26 15:19:34 +0300

    Allocate exceptions using an intrinsic function instead of allocating on heap

--- src/ThrowStatement.cpp
+++ src/ThrowStatement.cpp
@@ -8,7 +8,6 @@
 
 #include <llvm/IR/Constants.h>
 #include <llvm/IR/Instructions.h>
-#include <llvm/IR/TypeBuilder.h>
 
 #include "yazyk/IntrinsicFunctions.hpp"
 #include "yazyk/Log.hpp"
@@ -26,8 +25,9 @@ Value* ThrowStatement::generateIR(IRGenerationContext& context) const {
   }
 
   LLVMContext& llvmContext = context.getLLVMContext();
+  BasicBlock* basicBlock = context.getBasicBlock();
   context.getScopes().getScope()->addException(expressionType);
-  
+
   Model* model = dynamic_cast<Model*>(expressionType);
   GlobalVariable* rtti = context.getModule()->getGlobalVariable(model->getRTTIVariableName());
 
@@ -36,19 +36,38 @@ Value* ThrowStatement::generateIR(IRGenerationContext& context) const {
   BitCastInst* expressionValueBitcast = new BitCastInst(expressionValue,
                                                         int8PointerType,
                                                         "",
-                                                        context.getBasicBlock());
-  BitCastInst* rttiBitcast = new BitCastInst(rtti, int8PointerType, "", context.getBasicBlock());
-  
-  vector<Value*> arguments;
-  arguments.push_back(expressionValueBitcast);
-  arguments.push_back(rttiBitcast);
-  arguments.push_back(ConstantPointerNull::get(int8PointerType));
+                                                        basicBlock);
+  BitCastInst* rttiBitcast = new BitCastInst(rtti, int8PointerType, "", basicBlock);
+
+  Value* modelSize = model->getSize(context);
+  vector<Value*> allocateExceptionArguments;
+  allocateExceptionArguments.push_back(modelSize);
+  Function* allocateExceptionFunction = IntrinsicFunctions::getAllocateExceptionFunction(context);
+  Value* exceptionAlloca = CallInst::Create(allocateExceptionFunction,
+                                            allocateExceptionArguments,
+                                            "",
+                                            basicBlock);
+
+  vector<Value*> memCopyArguments;
+  memCopyArguments.push_back(exceptionAlloca);
+  memCopyArguments.push_back(expressionValueBitcast);
+  memCopyArguments.push_back(modelSize);
+  memCopyArguments.push_back(ConstantInt::get(Type::getInt32Ty(llvmContext), 8));
+  memCopyArguments.push_back(ConstantInt::get(Type::getInt1Ty(llvmContext), 0));
+  Function* memCopyFunction = IntrinsicFunctions::getMemCopyFunction(context);
+  CallInst::Create(memCopyFunction, memCopyArguments, "", basicBlock);
+
+  vector<Value*> throwArguments;
+  throwArguments.push_back(exceptionAlloca);
+  throwArguments.push_back(rttiBitcast);
+  throwArguments.push_back(ConstantPointerNull::get(int8PointerType));
   ExpressionList::const_iterator it;
-  
+
   Function* throwFunction = IntrinsicFunctions::getThrowFunction(context);
-  CallInst* callInst = CallInst::Create(throwFunction, arguments, "", context.getBasicBlock());
-  
-  new UnreachableInst(llvmContext, context.getBasicBlock());
-  
+
+  CallInst* callInst = CallInst::Create(throwFunction, throwArguments, "", basicBlock);
+
+  new UnreachableInst(llvmContext, basicBlock);
+
   return callInst;
 }
