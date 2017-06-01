b742f70d8b2eaf741c4f7d8457feb56a4253cb94
Vladimir Fridman <vlad.fridman@gmail.com>
2017-06-01 11:50:30 +0300

    Rename some test samples directory

--- tests/TestMain.cpp
+++ tests/TestMain.cpp
@@ -74,24 +74,24 @@ TEST_F(TestMain, runTest) {
 }
 
 TEST_F(TestMain, runMultipleFilesTest) {
-  string result = exec("bin/wisey tests/samples/test_multifile/Adder.yz "
-                       "tests/samples/test_multifile/Runner.yz");
+  string result = exec("bin/wisey tests/samples/test_multifile_controller_controller/Adder.yz "
+                       "tests/samples/test_multifile_controller_controller/Runner.yz");
   
   string lastLine = result.substr(result.find("Info: Result"));
   EXPECT_STREQ(lastLine.c_str(), "Info: Result: 5\n");
 }
 
 TEST_F(TestMain, runMultipleFilesOutOfOrderTest) {
-  string result = exec("bin/wisey tests/samples/test_multifile/Runner.yz "
-                       "tests/samples/test_multifile/Adder.yz");
+  string result = exec("bin/wisey tests/samples/test_multifile_controller_controller/Runner.yz "
+                       "tests/samples/test_multifile_controller_controller/Adder.yz");
   
   string lastLine = result.substr(result.find("Info: Result"));
   EXPECT_STREQ(lastLine.c_str(), "Info: Result: 5\n");
 }
 
 TEST_F(TestMain, runMultipleFilesControllerAndModelsTest) {
-  string result = exec("bin/wisey tests/samples/test_multifile_interdependent/MAdder.yz "
-                       "tests/samples/test_multifile_interdependent/CProgram.yz");
+  string result = exec("bin/wisey tests/samples/test_multifile_controller_model/MAdder.yz "
+                       "tests/samples/test_multifile_controller_model/CProgram.yz");
   
   string lastLine = result.substr(result.find("Info: Result"));
   EXPECT_STREQ(lastLine.c_str(), "Info: Result: 7\n");
