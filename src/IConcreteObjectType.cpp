4d7b3b9f3fccb8cc321eaddc3a18d7007154298e
Vladimir Fridman <vlad.fridman@gmail.com>
2018-04-18 13:55:25 +0300

    Do not create field variables for llvm functions

--- src/IConcreteObjectType.cpp
+++ src/IConcreteObjectType.cpp
@@ -336,9 +336,6 @@ void IConcreteObjectType::declareFieldVariables(IRGenerationContext& context,
   for (IField* field : object->getFields()) {
     field->getType()->createFieldVariable(context, field->getName(), object);
   }
-  for (LLVMFunction* function : object->getLLVMFunctions()) {
-    function->getType()->createFieldVariable(context, function->getName(), object);
-  }
 }
 
 void IConcreteObjectType::composeDestructorBody(IRGenerationContext& context,
