f2ccc7433b2874b2bbdde6e36b1702dd456b7fa0
Vladimir Fridman <vlad.fridman@gmail.com>
2018-05-23 01:47:42 +0300

    Introduce CReferenceVector

--- tests/TestCReferenceVector.cpp
+++ tests/TestCReferenceVector.cpp
@@ -1,28 +1,28 @@
 //
-//  TestCOwnerVector.cpp
+//  TestCReferenceVector.cpp
 //  runtests
 //
-//  Created by Vladimir Fridman on 5/22/18.
+//  Created by Vladimir Fridman on 5/23/18.
 //  Copyright © 2018 Vladimir Fridman. All rights reserved.
 //
-//  Tests {@link wisey.data.COwnerVector}
+//  Tests {@link wisey.data.CReferenceVector}
 //
 
 #include <gtest/gtest.h>
 
 #include "TestFileRunner.hpp"
 
-TEST_F(TestFileRunner, cOwnerVectorRunTest) {
-  runFileCheckOutputWithDestructorDebug("tests/samples/test_cownervector.yz",
+TEST_F(TestFileRunner, cReferenceVectorRunTest) {
+  runFileCheckOutputWithDestructorDebug("tests/samples/test_creferencevector.yz",
                                         "Vector size: 0, capacity: 3\n"
                                         "Vector size: 1, capacity: 3\n"
                                         "Vector size: 2, capacity: 3\n"
                                         "Vector size: 3, capacity: 3\n"
                                         "Vector size: 4, capacity: 6\n"
                                         "Car year: 2015\n"
-                                        "destructor systems.vos.wisey.compiler.tests.MCar\n"
                                         "Vector size: 3, capacity: 6\n"
                                         "Car year: 2018\n"
+                                        "destructor systems.vos.wisey.compiler.tests.MCar\n"
                                         "destructor systems.vos.wisey.compiler.tests.MCar\n"
                                         "destructor systems.vos.wisey.compiler.tests.MCar\n"
                                         "destructor systems.vos.wisey.compiler.tests.MCar\n"
