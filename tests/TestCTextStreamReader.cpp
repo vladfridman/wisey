6a1f209998c73801a80e7baea83f2eac9fb432f4
Vladimir Fridman <vlad.fridman@gmail.com>
2018-08-27 20:10:40 +0300

    Introduce CTextStreamReader instead of CInputReader and CFileReader

--- tests/TestCTextStreamReader.cpp
+++ tests/TestCTextStreamReader.cpp
@@ -1,19 +1,23 @@
 //
-//  TestCInputReader.cpp
+//  TestCTextStreamReader.cpp
 //  runtests
 //
-//  Created by Vladimir Fridman on 8/16/18.
+//  Created by Vladimir Fridman on 5/10/18.
 //  Copyright © 2018 Vladimir Fridman. All rights reserved.
 //
-//  Tests {@link wisey.io.CInputReader}
+//  Tests {@link wisey.io.CTextStreamReader}
 //
 
 #include <gtest/gtest.h>
 
 #include "TestFileRunner.hpp"
 
-TEST_F(TestFileRunner, cInputReaderRunTest) {
-  compileAndRunFileWithInputCheckOutput("tests/samples/test_cinput_reader.yz",
+TEST_F(TestFileRunner, cTextStreamFileReaderRunTest) {
+  runFileCheckOutput("tests/samples/test_ctext_stream_file_reader.yz", "one\ntwo\nthree\n", "");
+}
+
+TEST_F(TestFileRunner, cTextStreamSystemInReaderRunTest) {
+  compileAndRunFileWithInputCheckOutput("tests/samples/test_ctext_stream_systemin_reader.yz",
                                         1,
                                         "tests/samples/testinput.txt",
                                         "line: this"
